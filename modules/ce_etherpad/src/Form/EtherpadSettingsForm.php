5eaa91f2e4b055fd7e269cf7d442be1eda7e2852
Anant Jain <anantjain60@gmail.com>
2019-06-01 02:42:18 +0530

    Connection with Etherpad established

--- modules/ce_etherpad/src/Form/EtherpadSettingsForm.php
+++ modules/ce_etherpad/src/Form/EtherpadSettingsForm.php
@@ -44,6 +44,11 @@ class EtherpadSettingsForm extends ConfigFormBase {
       '#description' => $this->t('Enter Etherpad API Key. You can find API Key in APIKEY.txt on root directory of Etherpad.'),
       '#default_value' => $config->get('etherpad_api_key'),  
     ];  
+    $form['test_connection'] = array(
+      '#type' => 'submit',
+      '#value' => t('Test connection'),
+      '#submit' => array('::test_connection'),
+    );
 
     return parent::buildForm($form, $form_state);  
   } 
@@ -54,6 +59,32 @@ class EtherpadSettingsForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {  
     parent::submitForm($form, $form_state);  
 
+    $this->config('ce_etherpad.settings')  
+      ->set('etherpad_api_url', $form_state->getValue('etherpad_api_url'))  
+      ->set('etherpad_api_key', $form_state->getValue('etherpad_api_key'))  
+      ->save();  
+  }  
+
+  /**  
+   * {@inheritdoc}  
+   */  
+  public function test_connection(array &$form, FormStateInterface $form_state) {  
+    parent::submitForm($form, $form_state);  
+
+    try {
+      $client = new \EtherpadLite\Client($form_state->getValue('etherpad_api_key'), $form_state->getValue('etherpad_api_url'));
+      $response = $client->checkToken();
+
+      echo $response->getCode();
+      if($response->getMessage() == "ok")
+        drupal_set_message("Connection established successfully.");
+      else
+        drupal_set_message("Either or both API URL and API Key is incorrect", "error");
+    } catch(\GuzzleHttp\Exception\ConnectException $e) {
+        drupal_set_message("Unable to connect API URL or the API URL is incorrect.", "error");
+    } catch(\GuzzleHttp\Exception\ClientException $e) {
+        drupal_set_message("Either or both API URL and API Key is incorrect", "error");
+    }
     $this->config('ce_etherpad.settings')  
       ->set('etherpad_api_url', $form_state->getValue('etherpad_api_url'))  
       ->set('etherpad_api_key', $form_state->getValue('etherpad_api_key'))  
